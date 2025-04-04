f98614b6917846e71bcb47042b36e57b4d2cb9fd
pathum414 <chinthakapathum863@gmail.com>
2025-04-04 22:28:39 +0530

    Update alive.js

--- Plugins/alive.js
+++ Plugins/alive.js
@@ -1,20 +1,8 @@
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
-
-cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
-    filename: __filename
-},
-async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-const config = await readEnv();
-return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
-    
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
-
+module.exports = {
+    name: "alive",
+    description: "Check if the bot is alive",
+    execute(client, message, args) {
+        const response = "✅ *I'm alive and running smoothly!* 🚀";
+        client.sendMessage(message.chatId, response);
+    }
+};
